0831debb24382b5a4af83fdd690b1f89e56713ca
typhainepl <typhaine@ebi.ac.uk>
2017-04-11 11:40:28 +0100

    Add time running program

--- mapping_process.pl
+++ mapping_process.pl
@@ -27,8 +27,7 @@ use get_gold_clusters;
 my %config = do './config.pl';    
 my $pdbe_dbh = DBI->connect("DBI:Oracle:".$config{db}, $config{user}, $config{password});
 
-my $datestart = localtime();
-print "Mapping process started at $datestart\n";
+print "Mapping process started\n";
 
 #delete and rename existing tables
 #print "drop tables\n";
@@ -51,10 +50,10 @@ foreach my $t (@tables){
 #$pdbe_dbh->do("drop table SEGMENT_SCOP_TEST") or die;
 #$pdbe_dbh->do("drop table PDBE_ALL_DOMAIN_MAPPING_TEST") or die;
 #$pdbe_dbh->do("drop table PDBE_ALL_NODE_MAPPING_TEST") or die;
-$pdbe_dbh->do("drop table BLOCK_CHAIN_TEST") or die;
-$pdbe_dbh->do("drop table BLOCK_UNIPROT_TEST") or die;
-$pdbe_dbh->do("drop table MDA_BLOCK_TEST") or die;
-$pdbe_dbh->do("drop table CLUSTER_BLOCK_TEST") or die;
+#$pdbe_dbh->do("drop table BLOCK_CHAIN_TEST") or die;
+#$pdbe_dbh->do("drop table BLOCK_UNIPROT_TEST") or die;
+#$pdbe_dbh->do("drop table MDA_BLOCK_TEST") or die;
+#$pdbe_dbh->do("drop table CLUSTER_BLOCK_TEST") or die;
 #$pdbe_dbh->do("drop table CLUSTER_TEST") or die;
 
 
@@ -100,8 +99,9 @@ get_mda_blocks::getMDABlocks($pdbe_dbh, $mdaDirectory, $representative, %tables_
 #print equivalent gold pairs cluster blocks
 #get_gold_clusters::get_gold_clusters($pdbe_dbh,$blockFile,$goldFile, $tables_new{'PDBE_ALL_NODE_MAPPING'});
 
-my $dateend = localtime();
-print "Mapping process finished at $dateend\n";
+my $runtime = time - $^T."\n";
+
+printf("\n\nTotal running time: %02d:%02d:%02d\n\n", int($runtime / 3600), int(($runtime % 3600) / 60), int($runtime % 60),"\n");
 
 
 sub cleanDirectory{
