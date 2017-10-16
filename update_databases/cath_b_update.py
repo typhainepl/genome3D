fc2fbcab6d2922ed33aec5736e22d1a3af7fead6
typhainepl <typhaine@ebi.ac.uk>
2017-10-16 13:31:17 +0100

    change schema name sifts

--- update_databases/cath_b_update.py
+++ update_databases/cath_b_update.py
@@ -164,7 +164,7 @@ pdbeconnection.commit()
 fnames.close()
 
 #get the description corresponding to the number from CATH_DOMAIN for class, architecture, topology and homology superfamily
-pdbecursor.execute("select distinct(cathcode),class,arch,topol,homol from sifts_admin_new.CATH_DOMAIN");
+pdbecursor.execute("select distinct(cathcode),class,arch,topol,homol from sifts_admin.CATH_DOMAIN");
 cathinfo = pdbecursor.fetchall();
 
 homologies = {}
@@ -193,7 +193,7 @@ domains=[]
 segments=[]
 
 print "Get data from files"
-pdbecursor.prepare("select name,source from sifts_admin_new.CATH_DOMAIN where domain= :domain")
+pdbecursor.prepare("select name,source from sifts_admin.CATH_DOMAIN where domain= :domain")
 # Read data from all.txt file
 for row in fdomains.readlines():
 	# data for domain table
